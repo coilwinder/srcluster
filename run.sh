b1703b290df2f1619ec322baa0242b655f0f3798
Vladimir Zhukov <vl.zhukov@mail.ru>
2017-09-24 17:28:28 +0000

    version 1.3.2

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ SSH_CMD="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SSH_S
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+$SSH_CMD "cd /home/ansible/work/semrush/srcluster; env"
 $SSH_CMD "cd /home/ansible/work/semrush/srcluster; ./provisioner.sh inventories/staging/"
 exit 0
 
