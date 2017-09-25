c6b523678e452035b014e5d0a2ccb5067b073e42
Vladimir Zhukov <vl.zhukov@mail.ru>
2017-09-25 13:25:40 +0000

    cosmetic changes

--- travis-staging.sh
+++ travis-staging.sh
@@ -1,39 +1,55 @@
-#!/bin/bash -xv
+#!/bin/bash
 
-LOCK_FILE='/home/ansible/.sr_lock'
-WORK_DIR='/home/ansible/srwork'
-SSH_STR='ansible@46.101.218.33'
-SSH_CMD="ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SSH_STR}"
+set -o errexit
+set -o pipefail
+set -o nounset
 
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+LOCK_FILE='/home/ansible/.sr_lock'
+WORK_DIR='/home/ansible/work/semrush/srwork-staging'
+SSH_STR='ansible@46.101.218.33'
+SSH_CMD="ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${SSH_STR}"
 
-if $SSH_CMD "test -f ${LOCK_FILE}"
-then
-    echo "Error: lock file exists"
-    exit 1
-fi
-
-$SSH_CMD "touch ${LOCK_FILE}" || exit_error "ERROR can't create lock file"
-
-function exit_error ()
-{
+function exit_error() {
    MSG="$1"
-   $SSH_CMD "cd ${WORK_DIR}; ./provisioner.sh inventories/staging deleted"
+
    echo "${MSG}"
-   $SSH_CMD "rm ${LOCK_FILE}"
+   $SSH_CMD "cd ${WORK_DIR} && ./provisioner.sh inventories/staging deleted"
+   $SSH_CMD "rm -f ${LOCK_FILE}"
+
    exit 1
 }
 
 trap "exit_error 'ERROR: exit by signal'" ERR SIGINT SIGTERM
 
-$SSH_CMD "mkdir -p ${WORK_DIR}; rm -rf ${WORK_DIR}/*" || exit_error "ERROR can't prepare workdir"
+# Lock file management
+if $SSH_CMD "test -f ${LOCK_FILE}"; then
+    echo "ERROR: lock file exists"
+    exit 1
+else
+    $SSH_CMD "touch ${LOCK_FILE}" || exit_error "ERROR: can't create lock file"
+fi
 
-tar -czp --exclude-vcs --file - . | $SSH_CMD "tar xzp --file - -C ${WORK_DIR}" || exit_error "ERROR can't copy "
+# Create or clean working directory
+$SSH_CMD "mkdir -p ${WORK_DIR}; rm -rf ${WORK_DIR}/*" \
+    || exit_error "ERROR: can't prepare workdir"
 
-$SSH_CMD "cd ${WORK_DIR}; ./provisioner.sh inventories/staging/"  || exit_error "ERROR on staging.provisioner step"
-$SSH_CMD "cd ${WORK_DIR}; ansible-playbook -i inventories/staging site.yaml" || exit_error "ERROR on staging.ansible step"
-$SSH_CMD "cd ${WORK_DIR}; ./provisioner.sh inventories/staging/ deleted"
+# Send repozitory to deploy server
+tar --exclude-vcs czpf - . | $SSH_CMD "tar -C ${WORK_DIR} xzpf -" \
+    || exit_error "ERROR: can't copy repository"
 
+# Run provisioner.sh script
+$SSH_CMD "cd ${WORK_DIR} && ./provisioner.sh inventories/staging" \
+    || exit_error "ERROR on provisioner step"
+
+# Run ansible playbook
+$SSH_CMD "cd ${WORK_DIR} && ansible-playbook -i inventories/staging site.yaml" \
+    || exit_error "ERROR on ansible step"
+
+# Remove staging droplets
+$SSH_CMD "cd ${WORK_DIR} && ./provisioner.sh inventories/staging deleted"
+
+# Remove lock file
 $SSH_CMD "rm ${LOCK_FILE}"
 
