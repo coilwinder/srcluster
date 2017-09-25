c65a8ddf89ee13105dfe343c39a247a23d4142d5
Vladimir Zhukov <vl.zhukov@mail.ru>
2017-09-25 13:39:12 +0000

    changed tar options

--- travis-production.sh
+++ travis-production.sh
@@ -35,8 +35,8 @@ $SSH_CMD "mkdir -p ${WORK_DIR}; rm -rf ${WORK_DIR}/*" \
     || exit_error "ERROR: can't prepare workdir"
 
 # Send repozitory to deploy server
-tar --exclude-vcs --create --gzip --preserve --file - . \
-    | $SSH_CMD "tar --directory ${WORK_DIR} --extract --gzip --preserve --file -" \
+tar --exclude-vcs --create --preserve-permissions --gzip  --file - . \
+    | $SSH_CMD "tar --directory ${WORK_DIR} --extract --gzip --preserve-permissions --file -" \
     || exit_error "ERROR: can't copy repository"
 
 # Run provisioner.sh script
